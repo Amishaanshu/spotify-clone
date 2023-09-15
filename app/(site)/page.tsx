a78090adb0583da08c05d23669e5a367708f0218
Amishaanshu <95044561+Amishaanshu@users.noreply.github.com>
2023-09-15 17:39:03 +0530

    song display

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,10 @@
- import Header from "@/components/Header"
+ import getSongs from "@/actions/getSongs";
+import Header from "@/components/Header"
 import ListItem from "@/components/ListItem"
-export default function Home() {
+import PageContent from "./components/PageContent";
+export const revalidate=0;
+export default  async function Home() {
+  const songs=await getSongs();
   return (
     <div className="
     bg-neutral-900
@@ -29,12 +33,12 @@ export default function Home() {
 <div className="flex justify-between items-center">
 <h1 className="text-white text-2xl font-semibold">Newest Songs</h1>
 </div>
-<div>
-  List Of Songs
-</div>
+
+<PageContent songs={songs}/>
 
       </div>
     
       </div>
   )
 }
+ 
